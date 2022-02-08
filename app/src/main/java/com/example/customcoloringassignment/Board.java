faa2bdb6ca2e26801a6680219a98c0a8e92c09ad
7angsan <leeste25@up.edu>
2022-02-08 10:08:23 -0800

    Initial commit

---
+++ app/src/main/java/com/example/customcoloringassignment/Board.java
@@ -0,0 +1,11 @@
+package com.example.customcoloringassignment;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class Board extends SurfaceView {
+    public Board(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
